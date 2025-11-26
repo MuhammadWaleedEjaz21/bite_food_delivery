2799b1fa7e88df4c9a2db5b1fc6b39c7f5922ae7
MuhammadWaleedEjaz21 <m.waleedejaz2003@gmail.com>
2025-11-26 16:37:53 +0500

    feat: Refactor CategoryRestaurantMenu to use category provider for dynamic dropdown entries

--- lib/Screen/category_restaurant_menu.dart
+++ lib/Screen/category_restaurant_menu.dart
@@ -1,20 +1,33 @@
+import 'package:bite_food_delivery/Provider/category_provider.dart';
 import 'package:bite_food_delivery/Widget/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryRestaurantMenu extends ConsumerWidget {
   const CategoryRestaurantMenu({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final categoryAsync = ref.watch(categoryProvider);
     return Scaffold(
       appBar: CustomAppBar(
-        title: DropdownMenu(
-          dropdownMenuEntries: [
-            DropdownMenuEntry(value: 'one', label: 'Category 1'),
-            DropdownMenuEntry(value: 'two', label: 'Category 2'),
-            DropdownMenuEntry(value: 'three', label: 'Category 3'),
-          ],
+        title: categoryAsync.when(
+          data: (categories) => DropdownMenu(
+            hintText: 'Select Category',
+            menuStyle: MenuStyle(),
+            dropdownMenuEntries: categories
+                .map(
+                  (category) => DropdownMenuEntry(
+                    value: category.name,
+                    label: category.name,
+                  ),
+                )
+                .toList(),
+          ),
+          loading: () => const Text('Loading...'),
+          error: (error, stack) => const Text('Error'),
         ),
+        trailing: [90.horizontalSpace],
       ),
     );
   }
